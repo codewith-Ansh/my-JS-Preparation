881a496cc27e67b0f2b0bb1dbf54e26aa5ef54df
Ansh Darji <anshh.ce@gmail.com>
2025-06-15 22:10:54 +0530

    Update 04_Comparision.js

--- 01_Basics/04_Comparision.js
+++ 01_Basics/04_Comparision.js
@@ -12,7 +12,7 @@
 // console.log(null < 0)    // false
 // console.log(null >= 0)   // true
 // console.log(null <= 0)   // true
-// console.log(null == 0)   // true
+// console.log(null == 0)   // false
 
 /* The reason is that an equality check == and comparisons > < >= <= work differently.
 Comparisons convert null to a number, treating it as 0.
