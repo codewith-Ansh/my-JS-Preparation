43ace9ea9b76be93cfab95cdc9260498ba7e0c9f
codewith-Ansh <anshh.ce@gmail.com>
2025-09-17 22:54:47 +0530

    Update with few changes

--- 10_OOP/Getter_Setter.js
+++ 10_OOP/Getter_Setter.js
@@ -20,6 +20,8 @@ class User {
     }
 
     // _ is used before the variable name in getter and setter function to avoid the stack limit exceed error
+    // If you write this.email = value inside the set email(),
+    // it will call the setter again, because this.email is bound to the getter/setter itself. amd it will leads to infinite recursion
 }
 
 const Ansh = new User("Ansh@gmail.com", "abc")
